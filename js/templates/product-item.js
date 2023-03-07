39b8484f8ef132832954c77b7a58fb9cc80d2f44
Yana <yanaegorova69.3@gmail.com>
2023-03-08 00:07:57 +0200

    +

--- js/templates/product-item.js
+++ js/templates/product-item.js
@@ -19,9 +19,9 @@ export const item = (product) => {
       $${(product.price).toFixed(2)}
       </span>
 
-      <a href="product-page.html?id=${product.id}" class="btn btn--first" style="margin: ${product.isItRing ? 'auto' : ''}">details</a>
+      <a href="product-page.html?id=${product.id}" class="btn btn--first" style="margin: ${product.type == "ring" || product.type == "clothing" ? 'auto' : ''}">details</a>
       ${
-        product.isItRing ? '' : '<button class="btn js_add-to-cart">buy</button>'
+        product.type == "ring" || product.type == "clothing" ? '' : '<button class="btn js_add-to-cart">buy</button>'
       }
       </div>
 
